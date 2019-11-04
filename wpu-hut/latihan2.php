d3a65a89b406c170daf537379a439365172e9487
agus94 <agusismail94@gmail.com>
2019-11-04 14:25:29 +0700

    bekerja dg JSON

--- wpu-hut/latihan2.php
+++ wpu-hut/latihan2.php
@@ -1,10 +1,3 @@
-<?php 
-$data = file_get_contents('data/pizza.json');
-$menu = json_decode($data, true);
-
-$menu = $menu["menu"];
-
-?>
 
 
 <!doctype html>
@@ -31,9 +24,12 @@ $menu = $menu["menu"];
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home</a>
-            
-            </div>
+            <a class="nav-item nav-link active" href="#">All Menu</a>
+            <a class="nav-item nav-link" href="#">Pizza</a>
+            <a class="nav-item nav-link" href="#">Pasta</a>
+            <a class="nav-item nav-link" href="#">Nasi</a>
+            <a class="nav-item nav-link" href="#">Minuman</a>
+        </div>
         </div>
     </div>
     </nav>
@@ -46,24 +42,11 @@ $menu = $menu["menu"];
             </div>
         </div>
 
-        <div class="row">
-            <?php foreach($menu as $row) : ?>
-            <div class="col-md-4">
-                <div class="card mb-3">
-                <img src="img/menu/<?= $row["gambar"]; ?>" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $row["nama"]; ?></h5>
-                    <p class="card-text"><?= $row["deskripsi"]; ?></p>
-                    <h5 class="card-title">Rp. <?= $row["harga"]; ?></h5>
-                    <a href="#" class="btn btn-primary">Pesan sekarang</a>
-                </div>               
-            </div>
+        <div class="row" id="daftar-menu">
+
         </div>
-        <?php endforeach; ?>
     
     </div>
-
-
 
 
 
@@ -74,5 +57,6 @@ $menu = $menu["menu"];
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
   </body>
 </html>
