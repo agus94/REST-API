1e7c5ca202cde443b72941d78bd50aec9a78ac9c
agus94 <agusismail94@gmail.com>
2019-11-04 21:30:00 +0700

    apa itu public api

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -21,7 +21,7 @@ function searchMovie() {
                             <div class="card-body">
                             <h5 class="card-title">` + data.Title + `</h5>
                             <h6 class="card-subtitle mb-2 text-muted">` + data.Year + `</h6>
-                            <a href="#" class="card-link">See Detail</a>
+                            <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+ data.imdbID +`">See Detail</a>
                             </div>
                         </div>
                     </div>
@@ -29,7 +29,6 @@ function searchMovie() {
                 });
 
                 $('#search-input').val('');
-
 
             } else {
                 $('#movie-list').html(`
@@ -50,4 +49,41 @@ $('#search-input').on('keyup', function (e) {
     if (e.keyCode === 13) {
         searchMovie();
     }
+})
+
+
+$('#movie-list').on('click', '.see-detail', function() {
+    
+    $.ajax({
+        url: 'http://www.omdbapi.com',
+        dataType: 'json',
+        type: 'get',
+        data: {
+            'apikey': 'd07b2fea',
+            'i': $(this).data('id'),
+        },
+        success: function(movie) {
+            if( movie.Response === "True" ){
+                $('.modal-body').html(`
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="`+ movie.Poster +`" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="list-group">
+                                <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                                <li class="list-group-item">`+ movie.Title +`</li>
+                                <li class="list-group-item">`+ movie.Released +`</li>
+                                <li class="list-group-item">`+ movie.Genre +`</li>
+                                <li class="list-group-item">`+ movie.Director +`</li>
+                                <li class="list-group-item">`+ movie.Actors +`</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                `);
+            }
+        }
+    })
 })
